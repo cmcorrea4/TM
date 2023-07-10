faae275bdce4f162a5d45fff1ea238ec9387c9c5
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-10 16:36:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if img_file_buffer is not None:
     size = (224, 224)
 
     # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
     # Load the image into the array
     data[0] = normalized_image_array
 

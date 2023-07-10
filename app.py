02ed0adeaa9a9a80ea60f0b9733b6a766138bd3d
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-10 16:28:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ from keras.models import load_model
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento óptico de Caracteres")
+st.title("Reconocimiento de Imágenes")
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+   #To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
 
     size = (224, 224)
-    imag2 = ImagOps.fit(cv2_img, size,Imag.ANTIALIAS)
 
-    #turn the image into a numpy array
-    image_array = np.asarray(imag2)
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
     # Load the image into the array

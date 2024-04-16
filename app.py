155adba0ac2fb00d753a7d3cb11a49c3cba743d5
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-04-16 14:02:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if img_file_buffer is not None:
       st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
       st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
-    if prediction[0][2]>0.5:
-      st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
+    #if prediction[0][2]>0.5:
+    # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
 

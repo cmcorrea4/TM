22bf3f976c49848cb8669c1c67d17d073e1c1d7b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-06-02 15:55:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
-
+image = Image.open('OIG5.jpg')
+st.image(image, width=350)
+with st.sidebar:
+    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
 img_file_buffer = st.camera_input("Toma una Foto")
 
 if img_file_buffer is not None:

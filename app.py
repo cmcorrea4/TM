3c0af4e0b17af882a38d648441e44018c5858d62
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-09-04 21:17:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
-st.write("Versión de Python:", platform.python_version())
+#st.write("Versión de Python:", platform.python_version())
 image = Image.open('OIG5.jpg')
 st.image(image, width=350)
 with st.sidebar:
